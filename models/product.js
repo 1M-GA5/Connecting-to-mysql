a03afb6578438af8c4f3ddbcc8c773a4e06e9385
1M-GA <athrv.gajbhiye.xxii@gmail.com>
2023-07-10 17:22:39 +0530

    first commit

--- models/product.js
+++ models/product.js
@@ -1,40 +1,29 @@
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
 
-const db = require('../util/database');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
+const sequelize = require('../util/database');
+
+
+const product = sequelize .define('product',{
+  id:{
+    type : Sequelize.INTEGER,
+    autoIncrement : true,
+    allowNull : false,
+    primaryKey:true
+  },
+  title:Sequelize.STRING,
+  price :{
+    type : Sequelize.DOUBLE,
+    allowNull:false
+  },
+  imageUrl:{
+    type : Sequelize.STRING,
+    allowNull:false
+  },
+  description:{
+    type:Sequelize.STRING,
+    allowNull : false
   }
+});
 
-  save() {
-    return db.execute('INSERT INTO sys.products (title , price , imageUrl , description) VALUEs (?,?,?,?)',
-    [this.title,this.price,this.imageUrl,this.description]
-    
-    );
-  }
-
-  static deleteById(id) {
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id);
-      const updatedProducts = products.filter(prod => prod.id !== id);
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price);
-        }
-      });
-    });
-  }
-
-  static fetchAll() {
-     return db.execute('SELECT * FROM sys.products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM sys.products WHERE products.id = ?',[id])
-  }
-};
+module.exports = product;
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize =  require('sequelize');
 
-const pool = mysql.createPool({
-   host : 'localhost',
-   user:'root',
-   database : 'sys',
-   password : 'Mohit@123'
-});
 
-module.exports = pool.promise();
+const sequelize = new Sequelize('sys','root','Mohit@123',{dialect : 'mysql',host:'localhost'});
+
+
+
+module.exports = sequelize;
